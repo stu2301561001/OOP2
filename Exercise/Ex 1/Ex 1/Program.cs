10794804b3eff04cf4433ba60563953b08083036
toni panova <stoikapanova69@gmail.com>
2024-04-09 18:07:05 +0300

    change 1

--- Exercise/Ex 1/Ex 1/Program.cs
+++ Exercise/Ex 1/Ex 1/Program.cs
@@ -15,27 +15,36 @@
             //      }
             //      Console.WriteLine("Сумата на всички цели числа от 1 до {0} e: {1}" , N, sum);
             //Задача 2
-            Console.WriteLine("Enter number");
-            var number = int.Parse(Console.ReadLine());
-            var isPrime = true;
-            for (int i = 2; i <= Math.Sqrt(number); i++)
+            //          Console.WriteLine("Enter number");
+            //           var number = int.Parse(Console.ReadLine());
+            //           var isPrime = true;
+            //          for (int i = 2; i <= Math.Sqrt(number); i++)
+            //          {
+            //              if (number % i == 0)
+            //              {
+            //                  isPrime = false;
+            //                  break;
+            //               }
+            //
+            //               if (isPrime)
+            //
+            //                   Console.WriteLine("true");
+
+            //                else
+
+            //                   Console.WriteLine("false");
+
+            //           }
+
+
+            Console.WriteLine("Enter array");
+            string[] input = Console.ReadLine().Split(",");
+            decimal result = 0;
+            for(int i = 0; i < input.Length; i++)
             {
-                if (number % i == 0)
-                {
-                    isPrime = false; break;
-                
-
-                if (isPrime)
-
-                    Console.WriteLine("true");
-
-                else
-
-                    Console.WriteLine("false");
-                }
+                result += Decimal.Parse(input[i]);
             }
-
-
+            Console.WriteLine( "The sum is " + result);
         }
     }
 } 
